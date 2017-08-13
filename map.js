24158d633a665da7ce30f907a8acebfa82fbaff3
Firat Yalavuz <yalavuz@gmail.com>
2017-08-13 19:27:02 +0300

    fixed typo and added versions of operating systems and browsers

--- map.js
+++ map.js
@@ -12,7 +12,7 @@ var grid = new contrib.grid({rows: 12, cols: 12, screen: screen})
 var map = grid.set(0, 0, 12, 10, contrib.map, {label: 'Latest ' + limit + ' visitors'})
 var countryLog = grid.set(0, 10, 5, 2, contrib.log, { fg: "green", label: 'Country', height: "20%", tags: true, border: { type: "line", fg: "cyan"}});
 var browserLog = grid.set(5, 10, 3, 2, contrib.log, { fg: "green", label: 'Browser', height: "20%", tags: true, border: { type: "line", fg: "cyan"}});
-var opereatigSystemLog = grid.set(8, 10, 4, 2, contrib.log, { fg: "green", label: 'Countries', height: "20%", tags: true, border: { type: "line", fg: "cyan"}});
+var opereatigSystemLog = grid.set(8, 10, 4, 2, contrib.log, { fg: "green", label: 'Operating System', height: "20%", tags: true, border: { type: "line", fg: "cyan"}});
 screen.render();
 
 
@@ -33,7 +33,10 @@ var parser = new NginxParser('$remote_addr - $remote_user [$time_local] '
 parser.tail('/projects/blessed-contrib/examples/nginx.log', function (row) {
 	var uaParser = new UAparser(row.http_user_agent);
 	var browser = uaParser.getBrowser();
-	if (browser.name) {
+    if (browser.name) {
+        if (browser.version) {
+            browser.name += ' (' + browser.version + ')';
+        }
 		browserLog.log(browser.name);
 	} else {
 		browserLog.log('Unknow Browser');
@@ -41,6 +44,10 @@ parser.tail('/projects/blessed-contrib/examples/nginx.log', function (row) {
 
 	var os = uaParser.getOS();
 	if (os.name) {
+        if (os.version) {
+            os.name += ' (' + os.version + ')';
+        }
+
 		opereatigSystemLog.log(os.name);
 	} else {
 		opereatigSystemLog.log('Uknown OS');
